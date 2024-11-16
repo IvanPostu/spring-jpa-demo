6d27fc2590cb6dbaec0d02f37a872166250146e7
IvanPostu <>
2024-11-16 23:06:40 +0200

    Create project skeleton

---
+++ parent-project/demo1/src/test/java/com/ipostu/demo1/AppTest.java
@@ -0,0 +1,11 @@
+package com.ipostu.demo1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertTrue(true);
+    }
+}
