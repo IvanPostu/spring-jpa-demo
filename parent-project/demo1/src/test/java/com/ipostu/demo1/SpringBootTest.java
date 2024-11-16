43bc96d1528fd463b2e4856265c6a8b026cdaec0
IvanPostu <>
2024-11-17 00:13:44 +0200

    H2 demo, logging + H2 console

---
+++ parent-project/demo1/src/test/java/com/ipostu/demo1/SpringBootTest.java
@@ -0,0 +1,10 @@
+package com.ipostu.demo1;
+
+import org.junit.jupiter.api.Test;
+
+@org.springframework.boot.test.context.SpringBootTest
+public class SpringBootTest {
+    @Test
+    public void testContextLoads() {
+    }
+}
