a92877df46e15327d3adb09a5a26b1528963113e
IvanPostu <ipostu20000127@gmail.com>
2024-11-24 13:13:44 +0200

    enable flyway clean

--- parent-project/demo2-flyway/src/main/java/com/ipostu/demo2/flyway/Demo2FlywayApp.java
+++ parent-project/demo2-flyway/src/main/java/com/ipostu/demo2/flyway/Demo2FlywayApp.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo2FlywayApp {
     public static void main(String[] args) {
-        System.setProperty("spring.profiles.active", "local,default");
+        System.setProperty("spring.profiles.active", "local,default,clean");
         SpringApplication.run(Demo2FlywayApp.class, args);
     }
 }
