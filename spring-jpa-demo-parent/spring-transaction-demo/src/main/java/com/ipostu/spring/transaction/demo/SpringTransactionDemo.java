19716934705dd0c903eb8ceba5733f500c475f8c
IvanPostu <ipostu20000127@gmail.com>
2025-01-07 16:00:43 +0200

    xml driven transaction config

--- spring-jpa-demo-parent/spring-transaction-demo/src/main/java/com/ipostu/spring/transaction/demo/SpringTransactionDemo.java
+++ spring-jpa-demo-parent/spring-transaction-demo/src/main/java/com/ipostu/spring/transaction/demo/SpringTransactionDemo.java
@@ -2,8 +2,10 @@ package com.ipostu.spring.transaction.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:application-context.xml")
 public class SpringTransactionDemo {
 
     public static void main(String[] args) {
--- spring-jpa-demo-parent/spring-transaction-demo/src/main/java/com/ipostu/spring/transaction/demo/services/AccountService.java
+++ spring-jpa-demo-parent/spring-transaction-demo/src/main/java/com/ipostu/spring/transaction/demo/services/AccountService.java
@@ -1,13 +1,10 @@
 package com.ipostu.spring.transaction.demo.services;
 
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
 public interface AccountService {
 
-    @Transactional
+    //    @Transactional
     void updateAccountFunds();
 
-    @Transactional(propagation = Propagation.NESTED)
+    //    @Transactional(propagation = Propagation.NESTED)
     void addFundsToAccount(int amount);
 }
