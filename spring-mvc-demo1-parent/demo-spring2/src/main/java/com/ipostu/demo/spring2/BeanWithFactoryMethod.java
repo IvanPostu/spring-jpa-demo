7c1f860dfad06847915cfd1b1681f7cc50109c58
IvanPostu <ipostu20000127@gmail.com>
2025-02-10 22:57:12 +0200

    xml context poc

---
+++ spring-mvc-demo1-parent/demo-spring2/src/main/java/com/ipostu/demo/spring2/BeanWithFactoryMethod.java
@@ -0,0 +1,19 @@
+package com.ipostu.demo.spring2;
+
+public class BeanWithFactoryMethod {
+    private BeanWithFactoryMethod() {
+    }
+
+
+    public static BeanWithFactoryMethod createStaticMethod() {
+        return new BeanWithFactoryMethod();
+    }
+
+    private void initExample() {
+        System.out.println("init method");
+    }
+
+    private void destroyExample() {
+        System.out.println("destroy method");
+    }
+}
