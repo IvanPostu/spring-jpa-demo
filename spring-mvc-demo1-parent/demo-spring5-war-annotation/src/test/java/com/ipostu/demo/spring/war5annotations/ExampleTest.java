91f7d3bfdabca5c7328788c099bcd2bc7dbbb033
IvanPostu <ipostu20000127@gmail.com>
2025-02-15 18:32:45 +0200

    #

---
+++ spring-mvc-demo1-parent/demo-spring5-war-annotation/src/test/java/com/ipostu/demo/spring/war5annotations/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.ipostu.demo.spring.war5annotations;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testQ() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
