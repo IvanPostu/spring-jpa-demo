de7775988a1675de428e93e35d52fef26a2c37e9
IvanPostu <ipostu20000127@gmail.com>
2025-02-15 15:32:37 +0200

    #

--- spring-mvc-demo1-parent/demo-spring4-war-annotation/src/main/java/com/ipostu/demo/spring/war4annotations/HelloController.java
+++ spring-mvc-demo1-parent/demo-spring4-war-annotation/src/main/java/com/ipostu/demo/spring/war4annotations/HelloController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,8 +21,12 @@ public class HelloController {
     }
 
     @GetMapping("/hello2")
-    public String hello2(@RequestParam(value = "name", required = false) String name) {
+    public String hello2(@RequestParam(value = "name", required = false) String name,
+                         Model model) {
         LOG.debug("Name: {}", name);
+
+        model.addAttribute("message",
+                "Hello " + name + " !!!");
         return "hello_world";
     }
 
