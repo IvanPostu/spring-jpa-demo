f5db93909e082bddfae16351785614de59460f7d
IvanPostu <ipostu20000127@gmail.com>
2025-02-17 22:18:38 +0200

    lazy/eager loading with persistence context manipulations

--- spring-mvc-demo1-parent/demo-spring8-poc-hibernate/src/main/java/com/ipostu/demo/spring/war8demo/App.java
+++ spring-mvc-demo1-parent/demo-spring8-poc-hibernate/src/main/java/com/ipostu/demo/spring/war8demo/App.java
@@ -1,6 +1,7 @@
 package com.ipostu.demo.spring.war8demo;
 
 import jakarta.persistence.Query;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -19,7 +20,8 @@ public class App {
                 .addAnnotatedClass(Item.class);
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
-             Session session = sessionFactory.getCurrentSession();) {
+        ) {
+            Session session = sessionFactory.getCurrentSession();
 
             session.beginTransaction();
 
@@ -28,9 +30,75 @@ public class App {
 //            insertActorWithItems(session);
 //            insertActorWithPassport(session);
 //            insertActorWithPassport2(session);
-
-            session.getTransaction().commit();
+//            useLazyEntitiesOutsideOfPersistenceContext(session, sessionFactory);
+//            reatachDetachedEntityToAnotherPersistenceContext(session, sessionFactory);
         }
+    }
+
+    private static void reatachDetachedEntityToAnotherPersistenceContext(Session session, SessionFactory sessionFactory) {
+        Movie movie = new Movie("Movie example", 2002);
+        Actor actor1 = new Actor("Actor 1 Name", 44);
+        Actor actor2 = new Actor("Actor 2 Name", 22);
+
+        movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
+        actor1.setMovies(new ArrayList<>(List.of(movie)));
+        actor2.setMovies(new ArrayList<>(List.of(movie)));
+
+        session.save(movie);
+        session.save(actor1);
+        session.save(actor2);
+
+        session.getTransaction().commit();
+
+
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        movie = session.merge(movie);
+
+        Hibernate.initialize(movie.getActors());
+        // System.out.println(movie2.getActors()); similar to Hibernate.initialize(movie2.getActors());
+        // movie2.getActors() won't work because java will optimize/remove this call because it isn't used
+
+        session.getTransaction().commit();
+
+        System.out.println(movie.getActors());
+    }
+
+    private static void useLazyEntitiesOutsideOfPersistenceContext(Session session, SessionFactory sessionFactory) {
+        Movie movie = new Movie("Movie example", 2002);
+        Actor actor1 = new Actor("Actor 1 Name", 44);
+        Actor actor2 = new Actor("Actor 2 Name", 22);
+
+        movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
+        actor1.setMovies(new ArrayList<>(List.of(movie)));
+        actor2.setMovies(new ArrayList<>(List.of(movie)));
+
+        session.save(movie);
+        session.save(actor1);
+        session.save(actor2);
+
+        session.getTransaction().commit();
+
+
+        session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+
+        Movie movie2 = session.get(Movie.class, movie.getId());
+
+        System.out.println(
+                session.createQuery("SELECT i FROM Item i WHERE i.owner.id=:personId", Item.class)
+                        .setParameter("personId", actor1.getId())
+                        .getResultList()
+        );
+
+        Hibernate.initialize(movie2.getActors());
+        // System.out.println(movie2.getActors()); similar to Hibernate.initialize(movie2.getActors());
+        // movie2.getActors() won't work because java will optimize/remove this call because it isn't used
+
+        session.getTransaction().commit();
+
+        System.out.println(movie2.getActors());
     }
 
     private static void insertActorWithPassport2(Session session) {
