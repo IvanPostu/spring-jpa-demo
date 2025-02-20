e0f2150f8ab9d7f48d2cfa2b2d9da344beca348c
IvanPostu <ipostu20000127@gmail.com>
2025-02-20 22:35:39 +0200

    create a clone of prev. spring app using spring boot

---
+++ spring-mvc-demo1-parent/demo-spring11-jar-boot/src/main/java/com/ipostu/demo/spring/jar11demo/models/Person.java
@@ -0,0 +1,117 @@
+package com.ipostu.demo.spring.jar11demo.models;
+
+
+import com.ipostu.demo.spring.jar11demo.validators.NoJohnValue;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @NoJohnValue(message = "Name shouldn't start with 'John' !")
+    @Column(name = "name")
+    private String name;
+
+    @Min(value = 0, message = "Age should be greater than 0")
+    @Column(name = "age")
+    private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
+    @Column(name = "email")
+    private String email;
+
+    // Country, City, Index (6 цифр)
+    // USA, Phoenix, 123456
+    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{6}",
+            message = "Your address should be in this format: Country, City, Postal Code (6 digits)")
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "date_of_birth")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "dd/MM/yyy")
+    private Date dateOfBirth;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    public Person() {
+    }
+
+    public Person(int id, String name, int age, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
---
+++ spring-mvc-demo1-parent/demo-spring11-jar-boot/src/main/java/com/ipostu/demo/spring/jar11demo/util/PersonValidator.java
@@ -0,0 +1,35 @@
+package com.ipostu.demo.spring.jar11demo.util;
+
+import com.ipostu.demo.spring.jar11demo.models.Person;
+import com.ipostu.demo.spring.jar11demo.repositories.PeopleRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PeopleRepository peopleRepository;
+
+    public PersonValidator(PeopleRepository peopleRepository) {
+        this.peopleRepository = peopleRepository;
+    }
+
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+        if (peopleRepository.findByEmail(person.getEmail()).isPresent()) {
+            errors.rejectValue("email", "", "This email is already in use");
+        }
+
+        if (!Character.isUpperCase(person.getName().codePointAt(0)))
+            errors.rejectValue("name", "", "Name should start with a capital letter");
+    }
+}
