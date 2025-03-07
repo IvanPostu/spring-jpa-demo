ddc04e6ff93365816e367f78d349647cb484b7f6
IvanPostu <ipostu20000127@gmail.com>
2025-03-07 18:09:52 +0200

    doc

---
+++ kafka-demo1-parent/demo1-producer/src/main/java/com/iv/kafkademo1/demo1producer/producer/HelloKafkaProducer.java
@@ -0,0 +1,14 @@
+package com.iv.kafkademo1.demo1producer.producer;
+
+@Service
+public class HelloKafkaProducer {
+
+//    @Autowired
+//    private KafkaTemplate<String, String> kafkaTemplate;
+//
+//    public void sendHello(String name) {
+//        kafkaTemplate.send("t-hello", "Hello " + name);
+//    }
+
+}
+
