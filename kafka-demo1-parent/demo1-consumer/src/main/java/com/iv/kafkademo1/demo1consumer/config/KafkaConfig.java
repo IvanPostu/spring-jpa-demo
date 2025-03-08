10ea176f50929d6014198207eb6ef96b11d64936
IvanPostu <ipostu20000127@gmail.com>
2025-03-08 18:24:01 +0200

    kafka filtering demo

--- kafka-demo1-parent/demo1-consumer/src/main/java/com/iv/kafkademo1/demo1consumer/config/KafkaConfig.java
+++ kafka-demo1-parent/demo1-consumer/src/main/java/com/iv/kafkademo1/demo1consumer/config/KafkaConfig.java
@@ -1,19 +1,28 @@
 package com.iv.kafkademo1.demo1consumer.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.iv.kafkademo1.demo1consumer.entity.CarLocation;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
-import org.springframework.boot.ssl.SslBundles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.adapter.RecordFilterStrategy;
 
 @Configuration
 public class KafkaConfig {
 
     @Autowired
     private KafkaProperties kafkaProperties;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Bean
     public ConsumerFactory<Object, Object> consumerFactory() {
@@ -23,6 +32,29 @@ public class KafkaConfig {
         properties.put(ConsumerConfig.METRICS_SAMPLE_WINDOW_MS_CONFIG, "40000");
 
         return new DefaultKafkaConsumerFactory<>(properties);
+    }
+
+    @Bean(name = "farLocationContainerFactory")
+    public ConcurrentKafkaListenerContainerFactory<Object, Object> farLocationContainerFactory(
+            ConcurrentKafkaListenerContainerFactoryConfigurer configurer) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<Object, Object>();
+        configurer.configure(factory, consumerFactory());
+
+        factory.setRecordFilterStrategy(new RecordFilterStrategy<Object, Object>() {
+
+            @Override
+            public boolean filter(ConsumerRecord<Object, Object> consumerRecord) {
+                try {
+                    CarLocation carLocation = objectMapper.readValue(consumerRecord.value().toString(),
+                            CarLocation.class);
+                    return carLocation.getDistance() <= 100;
+                } catch (JsonProcessingException e) {
+                    return false;
+                }
+            }
+        });
+
+        return factory;
     }
 
 }
--- kafka-demo1-parent/demo1-consumer/src/main/java/com/iv/kafkademo1/demo1consumer/consumer/RebalanceConsumer.java
+++ kafka-demo1-parent/demo1-consumer/src/main/java/com/iv/kafkademo1/demo1consumer/consumer/RebalanceConsumer.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
-@Service
+//@Service
 public class RebalanceConsumer {
 
     private static final Logger LOG = LoggerFactory.getLogger(RebalanceConsumer.class);
