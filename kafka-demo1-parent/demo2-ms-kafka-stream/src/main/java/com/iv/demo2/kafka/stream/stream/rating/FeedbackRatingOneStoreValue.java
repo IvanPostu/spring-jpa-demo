45cfa0436ac752fbbba9c8e0e50a0fb1e6b4c4ea
IvanPostu <ipostu20000127@gmail.com>
2025-04-04 23:18:55 +0300

    average rating

---
+++ kafka-demo1-parent/demo2-ms-kafka-stream/src/main/java/com/iv/demo2/kafka/stream/stream/rating/FeedbackRatingOneStoreValue.java
@@ -0,0 +1,30 @@
+package com.iv.demo2.kafka.stream.stream.rating;
+
+public class FeedbackRatingOneStoreValue {
+
+    private long countRating;
+    private long sumRating;
+
+    public long getCountRating() {
+        return countRating;
+    }
+
+    public long getSumRating() {
+        return sumRating;
+    }
+
+    public void setCountRating(long countRating) {
+        this.countRating = countRating;
+    }
+
+    public void setSumRating(long sumRating) {
+        this.sumRating = sumRating;
+    }
+
+    @Override
+    public String toString() {
+        return "FeedbackRatingOneStoreValue [countRating=" + countRating + ", sumRating=" + sumRating + "]";
+    }
+
+}
+
